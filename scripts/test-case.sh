379df4abb1d4c628a69094400a53ccf7ce7e3dee
Konstantin Köhring <konstantin.koehring@peerox.de>
2024-04-18 13:41:54 +0200

    Also probe google.com

--- scripts/test-case.sh
+++ scripts/test-case.sh
@@ -5,3 +5,8 @@ echo "Probing $HOSTIP"
 
 sudo podman exec netshoot curl --max-time 10 --interface eth0 -Ss -o /dev/null http://$HOSTIP && echo "eth0 OK" || echo "eth0 Error"
 sudo podman exec netshoot curl --max-time 10 --interface eth1 -Ss -o /dev/null http://$HOSTIP && echo "eth1 OK" || echo "eth1 Error"
+
+
+echo "Probing http://google.com"
+sudo podman exec netshoot curl --max-time 10 --interface eth0 -Ss -o /dev/null http://www.google.com && echo "eth0 OK" || echo "eth0 Error"
+sudo podman exec netshoot curl --max-time 10 --interface eth1 -Ss -o /dev/null http://www.google.com && echo "eth1 OK" || echo "eth1 Error"
